a5464cbc792c0d967255f512b3d872443c8104a2
TahaShameli <112875671+TahaShameli@users.noreply.github.com>
2025-02-08 22:55:38 +0330

    finished but still not responsive

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const btnFormTogglers = document.querySelectorAll(".form-toggler");
+const formsContainer = document.querySelector(".forms-container");
+
+btnFormTogglers.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    formsContainer.classList.toggle("toggle-form");
+  });
+});
